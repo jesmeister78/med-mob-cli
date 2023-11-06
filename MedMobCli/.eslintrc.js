069b7e1987df2120dfe073d3d4a2ec4b2cecae91
Jesse Outh-Aut <Jesse.Outh-Aut@wolterskluwer.com>
2023-11-06 14:51:45 +1100

    builds and runs in android emu with both react-navigation and react-vision-camera

--- MedMobCli/.eslintrc.js
+++ MedMobCli/.eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react",
+        "react-hooks"
+    ],
+    "rules": {
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
+    }
+}
--- MedMobCli/App.tsx
+++ MedMobCli/App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
-import React, {useEffect, useState, useRef} from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   Text,
   useColorScheme,
@@ -14,119 +14,36 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {Camera, useCameraDevice, useCameraDevices} from 'react-native-vision-camera';
+import { Camera, useCameraDevice, useCameraDevices } from 'react-native-vision-camera';
 
 /*
   local imports
 */
 import styles from './styles';
+import HomeScreen from './components/HomeScreen';
+import CaptureImage from './components/CaptureImage';
 
-function App(){
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // setup navigation
 
-  const camera = useRef<Camera>(null);
-  const devices = useCameraDevices();
-  console.log(devices);
-  const device = useCameraDevice('back');
-
-  const [showCamera, setShowCamera] = useState(false);
-  const [imageSource, setImageSource] = useState('');
-
-  useEffect(() => {
-    async function getPermission() {
-      const newCameraPermission = await Camera.requestCameraPermission();
-      console.log(newCameraPermission);
-    }
-    getPermission();
-  }, []);
-
-  const capturePhoto = async () => {
-    if (camera.current !== null) {
-      const photo = await camera.current.takePhoto({});
-      setImageSource(photo.path);
-      setShowCamera(false);
-      console.log(photo.path);
-    }
-  };
-
-  if (device == null) {
-    return <Text>Camera not available</Text>;
-  }
+  const Stack = createNativeStackNavigator();
+ 
   return (
-    
-    <View style={styles.container}>
-    {showCamera ? (
-      <>
-        <Camera
-          ref={camera}
-          style={styles.image}
-          device={device}
-          isActive={showCamera}
-          photo={true}
-        />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Capture" component={CaptureImage} />
+      </Stack.Navigator>
+    </NavigationContainer>
 
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.camButton}
-            onPress={() => capturePhoto()}
-          />
-        </View>
-      </>
-    ) : (
-      <>
-        {imageSource !== '' ? (
-          <Image
-            style={styles.image}
-            source={{
-              uri: `file://'${imageSource}`,
-            }}
-          />
-        ) : null}
-
-        <View style={styles.backButtonContainer}>
-          <TouchableOpacity
-            style={styles.backButton}
-            onPress={() => setShowCamera(true)}>
-            <Text style={{color: 'black', fontWeight: '500'}}>Back</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.buttonContainer}>
-          <View style={styles.buttons}>
-            <TouchableOpacity
-              style={styles.retakeButton}
-              onPress={() => setShowCamera(true)}>
-              <Text style={{color: '#77c3ec', fontWeight: '500'}}>
-                Retake
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.retakeButton}
-              onPress={() => setShowCamera(true)}>
-              <Text style={{color: '#77c3ec', fontWeight: '500'}}>
-                Save Image
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.usePhotoButton}
-              onPress={() => setShowCamera(true)}>
-              <Text style={{color: 'white', fontWeight: '500'}}>
-                Process Image
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </>
-    )}
-  </View>
   );
 }
 
